3db7497f404d41d7d0f54442024b7e40c2fed4d4
Nikita <lashkons@gmail.com>
2019-10-23 16:18:44 +0500

    + AA-tree

---
+++ KA/AA_tree/venv/Scripts/pip3.7-script.py
@@ -0,0 +1,12 @@
+#!C:\Users\TryUserPWNZ\AppData\Local\gitkraken\app-6.3.0\MexMaT\KA\AA_tree\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip3.7'
+__requires__ = 'pip==19.0.3'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==19.0.3', 'console_scripts', 'pip3.7')()
+    )
